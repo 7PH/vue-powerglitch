36fc828484b7228016598fb2181a4f62fa633237
7PH <b.raymond@protonmail.com>
2022-08-06 23:37:46 +0200

    Untrack build directory & Track eslint/gitignore/.vscode

--- build/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { App } from 'vue';
-import GlitchedImage from './GlitchedImage.vue';
-declare const _default: {
-    install: (vueApp: App) => void;
-};
-export default _default;
-export { GlitchedImage };
