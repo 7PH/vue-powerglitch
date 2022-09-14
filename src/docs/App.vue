9cde66b2fdf51a6bac1e3c5b7982f6ba39e7be0c
7PH <b.raymond@protonmail.com>
2022-09-14 23:32:33 +0200

    Adapt library to powerglitch@2.0.0

--- src/docs/App.vue
+++ src/docs/App.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import GlitchedImage from '@/GlitchedImage.vue';
+import { ref } from 'vue';
+import PowerGlitch from '../PowerGlitch.vue';
+
+const show = ref(true);
 </script>
 
 <template>
-    <GlitchedImage
-        src="https://raw.githubusercontent.com/7PH/vue-powerglitch/master/assets/logo.png"
-        class="logo"
-    />
+    <div
+        class="container"
+    >
+        <div>
+            Hello
+            <PowerGlitch
+                v-if="show"
+            >
+                World
+            </PowerGlitch> !
+        </div>
+        <div>
+            <button @click="show = ! show">
+                Click me
+            </button>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-.logo {
+.container {
     width: 100px;
     height: 100px;
     margin: 0 auto;
+}
+.logo {
+    width: 100%;
+    height: 100%;
 }
 </style>
